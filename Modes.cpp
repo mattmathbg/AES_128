0f13465e65c0439e35170aef3b5ae4d80fbbebab
Hugo_Plontz <hugo.plontz6@etu.univ-lorraine.fr>
2026-03-26 21:24:15 +0100

    dsq

--- Modes.cpp
+++ Modes.cpp
@@ -1,8 +1,8 @@
 #include "Modes.h"
 #include <stdexcept>
 
-// ─── Helpers PKCS#7 ─────────────────────────────────────────────────────────
 
+//fait en sorte que la taille du message soit un multiple de 16 octets pour pouvoir le chiffrer avec AES-128 (bloc de 16 octets)
 std::vector<uint8_t> Modes::pkcs7Pad(const std::vector<uint8_t>& data) {
     std::vector<uint8_t> padded = data;
     uint8_t padValue = 16 - (data.size() % 16);
@@ -13,32 +13,45 @@ std::vector<uint8_t> Modes::pkcs7Pad(const std::vector<uint8_t>& data) {
 }
 
 std::vector<uint8_t> Modes::pkcs7Unpad(const std::vector<uint8_t>& data) {
+    //Verfie que la taille du message est un multiple de 16 et que le padding est valide
     if (data.empty() || data.size() % 16 != 0) {
         throw std::runtime_error("Erreur PKCS#7 : taille invalide.");
     }
+    // padValue doit être entre 1 et 16 (jamais 0 et jamais plus de 16)
     uint8_t padValue = data.back();
     if (padValue == 0 || padValue > 16) {
         throw std::runtime_error("Erreur PKCS#7 : valeur de padding invalide.");
     }
+
+    //verifie si tout les octets de padding ont la même valeur que padValue (faut que ce soit cohérent)
     for (size_t i = data.size() - padValue; i < data.size(); i++) {
         if (data[i] != padValue) {
             throw std::runtime_error("Erreur PKCS#7 : padding incohérent.");
         }
     }
+    //on supprime le padding pour retourner le message original
     return std::vector<uint8_t>(data.begin(), data.end() - padValue);
 }
 
 // ─── ECB ─────────────────────────────────────────────────────────────────────
 
 std::vector<uint8_t> Modes::encryptECB(const std::vector<uint8_t>& plaintext, const AES128& aes) {
+    //on pad le message pour que sa taille soit un multiple de 16 octets
     std::vector<uint8_t> padded = pkcs7Pad(plaintext);
+
     std::vector<uint8_t> ciphertext;
+    //on reserve de la place pour le messagechiffré (optimisation pour éviter les reallocations)
     ciphertext.reserve(padded.size());
 
+    //on traite un bloc de 16 octets à la fois
     for (size_t i = 0; i < padded.size(); i += 16) {
+        //on crée un tableau fixe de 16 octets pour le bloc courant
         std::array<uint8_t, 16> block;
+        // Copie des 16 octets depuis padded vers block
         for (int j = 0; j < 16; j++) block[j] = padded[i + j];
+        //on chiffre le bloc avec AES-128
         aes.encryptBlock(block);
+        //on ajoute le bloc chiffré au résultat final
         for (int j = 0; j < 16; j++) ciphertext.push_back(block[j]);
     }
     return ciphertext;
